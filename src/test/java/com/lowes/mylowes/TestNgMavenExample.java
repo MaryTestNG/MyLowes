83d61bec4fa336925cd461b062a1d32bc444dbb6
kiran <kiranreddyamk@gmail.com>
2017-06-04 23:12:56 -0400

    Initial Commit

---
+++ src/test/java/com/lowes/mylowes/TestNgMavenExample.java
@@ -0,0 +1,11 @@
+package com.lowes.mylowes;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExample {
+
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+}
